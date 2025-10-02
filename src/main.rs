d53b4b9cf58d8721aea2f33c6c12ca0316ea92d5
Stonie <aronstein10@gmail.com>
2025-10-02 13:14:02 +0200

    feat: create window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use winit::event::{WindowEvent};
+use winit::event_loop::{EventLoopBuilder, ActiveEventLoop};
+use winit::window::{Window, WindowId};
+
+struct Brickbyte{
+    window: Option<Window>,
+}
+
+impl Brickbyte {
+    fn new() -> Self {
+        Brickbyte{window: None}
+    }
+}
+
+impl winit::application::ApplicationHandler for Brickbyte {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window_attributes = Window::default_attributes().with_title("Brickbyte");
+        self.window = Some(event_loop.create_window(window_attributes).unwrap());
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _window_id: WindowId, event: WindowEvent) {
+        if let WindowEvent::CloseRequested = event {
+            event_loop.exit();
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoopBuilder::default().build().unwrap();
+    let mut brickbyte = Brickbyte::new();
+    event_loop.run_app(&mut brickbyte).unwrap();
 }
